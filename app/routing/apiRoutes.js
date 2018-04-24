d1f585274804ee012aa83ac3fd8f6d0e7a4d9a54
Mapi Pina <mpina09@gmail.com>
2018-04-24 17:34:42 -0400

    messed up on the api call to generate JSON for friends list

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,11 +7,11 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
+  app.get("../data/friends.js", function(req, res) {
     res.json(besties);
   });
 
-  app.post("/api/friends", function(req, res) {
+  app.post("../data/friends.js", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
